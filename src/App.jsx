623542ebd511fd54399006a35b994937bd305a17
aspect19 <jeffamenya388@gmail.com>
2025-02-15 10:13:05 +0300

    initial Push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+const App = () => {
+
+  return (
+    <>
+      <div className="flex items-center justify-center  ">
+        <p className="p-5">
+          Initial screen after setup
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
