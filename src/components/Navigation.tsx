7c78612b73524a569ec83f4a8986771a247e1f71
aspect19 <jeffamenya388@gmail.com>
2025-04-22 15:10:46 +0300

    Fix Desktop navbar height

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,7 +10,7 @@ function Navigation() {
       setCollapsed(!collapsed)
     };
 
-    const scrollToTarget = (id) => {
+    const scrollToTarget = (id: string) => {
       const element = document.getElementById(id);
       if (element) {
         element.scrollIntoView({
@@ -18,40 +18,38 @@ function Navigation() {
           block: 'start',
         });
         setCollapsed(false);
-      } else {
-        console.log(`Element "$(id)" not found`)
-      }
+      } else {}
     };
 
   return (
-  <div className='fixed top-1 left-2 right-2 md:bg-[#10082565] md:backdrop-blur-2xl z-50'>
-    <div className=' md:hidden z-50 top-6 cursor-pointer' onClick={handleCollapse}>
-      {collapsed ? <IoMdClose size={IconSize}/> : <RxHamburgerMenu size={IconSize} />}
+    <div className='fixed top-1 left-2 right-2 md:bg-[#10082565] md:backdrop-blur-2xl z-50'>
+      <div className=' md:hidden z-50 top-6 cursor-pointer' onClick={handleCollapse}>
+        {collapsed ? <IoMdClose size={IconSize}/> : <RxHamburgerMenu size={IconSize} />}
+      </div>
+      <div className={` ${collapsed ? 
+        'fixed flex flex-col justify-around text-white items-center backdrop-blur-[4px] top-0 left-0 right-0 bottom-0 py-36 font-bold text-3xl *:cursor-pointer' : 
+        ' hidden text-4xl md:text-xl w-full md:flex flex-row pl-2 py-28 md:py-0 md:pl-16 md:top-2.5 md:h-12 items-center justify-around '}`}
+        >
+        <div className=' transition-colors cursor-pointer hover:underline hover:text-blue-300  ' onClick={() => scrollToTarget('home')}>
+          <span >Home</span>
+        </div>
+        <div className=' transition-colors cursor-pointer hover:underline hover:text-blue-300  ' onClick={() => scrollToTarget('about')}>
+          <span >About Me</span>
+        </div>
+        <div className=' transition-colors cursor-pointer hover:underline hover:text-blue-300  ' onClick={() => scrollToTarget('skills')}>
+          <span >Skills</span>
+        </div>
+        <div className=' transition-colors cursor-pointer hover:underline hover:text-blue-300  ' onClick={() => scrollToTarget('works')}>
+          <span >My Works</span>
+        </div>
+        <div className=' transition-colors cursor-pointer hover:underline hover:text-blue-300  ' onClick={() => scrollToTarget('reviews')}>
+          <span >Reviews</span>
+        </div>
+        <div className=' transition-colors cursor-pointer hover:underline hover:text-blue-300  ' onClick={() => scrollToTarget('contacts')}>
+          <span >Contact Me</span>
+        </div>
+      </div>
     </div>
-    <div className={` ${collapsed ? 
-      'fixed flex flex-col justify-around text-white items-center backdrop-blur-[4px] top-0 left-0 right-0 bottom-0 py-36 font-bold text-3xl *:cursor-pointer' : 
-      ' hidden text-4xl md:text-xl w-full md:flex flex-row pl-2 py-28 md:py-0 md:top-2.5 items-center justify-around '}`}
-      >
-      <div className=' transition-colors' onClick={() => scrollToTarget('home')}>
-        <span >Home</span>
-      </div>
-      <div className=' transition-colors' onClick={() => scrollToTarget('about')}>
-        <span >About Me</span>
-      </div>
-      <div className=' transition-colors' onClick={() => scrollToTarget('skills')}>
-        <span >Skills</span>
-      </div>
-      <div className=' transition-colors' onClick={() => scrollToTarget('works')}>
-        <span >My Works</span>
-      </div>
-      <div className=' transition-colors' onClick={() => scrollToTarget('reviews')}>
-        <span >Reviews</span>
-      </div>
-      <div className=' transition-colors' onClick={() => scrollToTarget('contacts')}>
-        <span >Contact Me</span>
-      </div>
-    </div>
-  </div>
   );
 };
 
